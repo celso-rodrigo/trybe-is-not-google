252c80748706bba8d2b6c8c27826d676e846d2a4
celso-rodrigo <celsorodrigoma@gmail.com>
2023-04-04 15:23:14 -0300

    feat: Concluído 8° requisito

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,4 +1,4 @@
-def get_word_occurrences(lines, word):
+def get_word_occurrences(lines, word, simplified):
     """
     Recives an list of string and returns a list of dicts
     with all occurrences or None
@@ -8,18 +8,21 @@ def get_word_occurrences(lines, word):
 
     for index in range(len(lines)):
         if word.lower() in lines[index].lower():
-            occurrences.append({"linha": index + 1})
+            if simplified:
+                occurrences.append({"linha": index + 1})
+            else:
+                occurrences.append({
+                    "linha": index + 1, "conteudo": lines[index]})
 
-    if len(occurrences) == 0:
+    if not occurrences:
         return None
 
     return occurrences
 
 
-def exists_word(word, instance):
+def handle_results(word, instance, simplified):
     """
-    Returns information about every time a word appears
-    in one value of the instance
+    Properly formats information about the number of occurrences of a word
     """
 
     if not word or not isinstance(word, str):
@@ -27,13 +30,13 @@ def exists_word(word, instance):
 
     results = list()
 
-    for index_data in range(len(instance)):
-        lines = instance.search(index_data)["linhas_do_arquivo"]
-        occurrences = get_word_occurrences(lines, word)
+    for index in range(len(instance)):
+        lines = instance.search(index)["linhas_do_arquivo"]
+        occurrences = get_word_occurrences(lines, word, simplified)
 
         if occurrences:
             results.append({
-                "arquivo": instance.search(index_data)["nome_do_arquivo"],
+                "arquivo": instance.search(index)["nome_do_arquivo"],
                 "ocorrencias": occurrences,
                 "palavra": word,
             })
@@ -41,5 +44,17 @@ def exists_word(word, instance):
     return results
 
 
+def exists_word(word, instance):
+    """
+    Returns simplified information about every time a word appears
+    in one value of the instance
+    """
+    return handle_results(word, instance, True)
+
+
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    """
+    Returns information about every time a word appears
+    in one value of the instance
+    """
+    return handle_results(word, instance, False)
