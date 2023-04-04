1a614a75817c3d0c896016e9b473f8b20a513b27
celso-rodrigo <celsorodrigoma@gmail.com>
2023-04-04 12:42:20 -0300

    feat: Concluído 3° requisito

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if path_file in instance.search(index).values():
+            return None
+
+    lines = txt_importer(path_file)
+    number_of_lines = len(lines)
+    process_content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": number_of_lines,
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(process_content)
+    print(process_content, file=sys.stdout)
 
 
 def remove(instance):
