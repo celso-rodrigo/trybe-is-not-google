ce0879b93aadd3028f7da219759d3b00b65a82ec
celso-rodrigo <celsorodrigoma@gmail.com>
2023-04-04 11:59:51 -0300

    feat: Concluído 2° requisito

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    """
+    Receives a path and returns a list with the content of the .txt
+    file on that path. If the file does not exist or is not a .txt
+    file, an error message will be printed.
+    """
+
+    if not path_file.endswith(".txt"):
+        return print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file, "r") as file:
+            content_file = file.read()
+            return list(content_file.split("\n"))
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
